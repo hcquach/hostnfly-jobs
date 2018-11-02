b3ef876b8e7d55452bd53737ec8c7c6359779b01
Hien-Chanh Quach <hcquach@gmail.com>
2018-11-02 12:34:38 +0100

    Removing unecessary test and comments

--- backend/backend_test.rb
+++ backend/backend_test.rb
@@ -111,31 +111,28 @@ def handle_cleanings(input)
       missions.push(mission_checkin, mission_checkout)
     end
 
-    # Testing if there are bookings
-    if bookings.any?
-      # Each loop going through all reservations
-      reservations.each do |reservation|
-        # Retrieving the listing associated with the booking
-        listing = listings.detect { |listing| listing[:id] == reservation[:listing_id] }
-        # Storing the number of rooms in a variable
-        number_rooms = listing[:num_rooms]
-        # Declaring a local variable to test if the end date of the booking and reservation is the same
-        same_end_date = false
-        # Retrieving all the bookings associated with the reservation
-        bookings_reservation = bookings.select { |booking| booking[:listing_id] == reservation[:listing_id] }
-        # Each loop to go through all bookings with the same listing id as reservation
-        bookings_reservation.each do |booking_reservation|
-          # Change the variable to true and break the loop if found any matching end_date
-          if booking_reservation[:end_date] == reservation[:end_date]
-            same_end_date = true
-            break
-          end
+    # Each loop going through all reservations
+    reservations.each do |reservation|
+      # Retrieving the listing associated with the booking
+      listing = listings.detect { |listing| listing[:id] == reservation[:listing_id] }
+      # Storing the number of rooms in a variable
+      number_rooms = listing[:num_rooms]
+      # Declaring a local variable to test if the end date of the booking and reservation is the same
+      same_end_date = false
+      # Retrieving all the bookings associated with the reservation
+      bookings_reservation = bookings.select { |booking| booking[:listing_id] == reservation[:listing_id] }
+      # Each loop to go through all bookings with the same listing id as reservation
+      bookings_reservation.each do |booking_reservation|
+        # Change the variable to true and break the loop if found any matching end_date
+        if booking_reservation[:end_date] == reservation[:end_date]
+          same_end_date = true
+          break
         end
-         # Test the end_date and add a new mission only if the end_date is not the same
-        if !same_end_date
-          mission_checkout_checkin = {:listing_id=>listing[:id], :mission_type=>"checkout_checkin", :date=>reservation[:end_date], :price=>number_rooms*10}
-          missions.push(mission_checkout_checkin)
-        end
+      end
+       # Test the end_date and add a new mission only if the end_date is not the same
+      if !same_end_date
+        mission_checkout_checkin = {:listing_id=>listing[:id], :mission_type=>"checkout_checkin", :date=>reservation[:end_date], :price=>number_rooms*10}
+        missions.push(mission_checkout_checkin)
       end
     end
 
